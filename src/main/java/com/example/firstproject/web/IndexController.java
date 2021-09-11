90754f5ca94a14464f56d6766383e3749d48c4da
leegyuhun <79880216+leegyuhun@users.noreply.github.com>
2021-09-11 20:48:27 +0900

    Initial commit

---
+++ src/main/java/com/example/firstproject/web/IndexController.java
@@ -0,0 +1,49 @@
+package com.example.firstproject.web;
+
+import com.example.firstproject.config.auth.LoginUser;
+import com.example.firstproject.config.auth.dto.SessionUser;
+import com.example.firstproject.service.posts.PostsService;
+import com.example.firstproject.service.posts.PostsService;
+import com.example.firstproject.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+//    @GetMapping
+//    public String index(){
+//        return "index";
+//    }
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user!=null){
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
