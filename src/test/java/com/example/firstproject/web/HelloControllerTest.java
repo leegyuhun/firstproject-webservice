6fc4e0bf00be942a126bf7133840d9d40a5e6b93
leegyuhun <79880216+leegyuhun@users.noreply.github.com>
2021-09-12 11:40:42 +0900

    ccc

--- src/test/java/com/example/firstproject/web/HelloControllerTest.java
+++ src/test/java/com/example/firstproject/web/HelloControllerTest.java
@@ -4,9 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+//import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -24,14 +26,9 @@ public class HelloControllerTest {
 
     @Test
     public void hello가_리턴된다() throws Exception {
-        //given
         String hello = "hello";
 
-        //when
-        ResultActions perform = mvc.perform(get("/hello"));
-
-        //then
-        perform
+        mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
